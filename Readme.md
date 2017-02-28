5bd3a008e32b4fc3cb23be762599a826197f1013
eyal-leviton <eyall4454@gmail.com>
2017-02-28 17:38:35 +0200

    Added basic server

--- Readme.md
+++ Readme.md
@@ -1,12 +1,12 @@
-Multi-editor
+#Multi-editor
 https://docs.google.com/document/d/1SCG4KcYc_hNzz-Mb_-vy3ykq5zOAvprzA25neAXNRJg/edit?usp=sharing
 
-Outline
+##Outline
 A googledoc like editor with collaboration capability.
 
-Stages:
+##Stages:
 
-Basic editor
+###Basic editor
 Implement a basic editor using HTML client side.
 Use innerHTML to present the text[1].
 To simplify cursor, use background color to mark where cursor is.
@@ -15,34 +15,34 @@ For each key or cursor move send to server using XMLHTTPRequest[3][4].
 Server will write the keys into a file.
 At this point support only left/right arrows to move.
 
-Advanced editor
+###Advanced editor
 Add buttons to modify style of selected text: color/bold/underline.
 Use <Shift><Left/Right Arrow> to mark, inject correct HTML tags.
 
-Calibration
+###Calibration
 Workout the server side.
 The server will store the document content as one large HTML string.
 The cursor for each client will be based on random id taken from a cookie in the following format:
-<div id="cursor_<id>" style…>X</div>, this will also show the cursor, also give a random color for each id, so that each cursor will be in different color.
+<div id="cursor_<id>" styleï¿½>X</div>, this will also show the cursor, also give a random color for each id, so that each cursor will be in different color.
 Moving the cursor will remove the div and put it in different location.
 Each modification of content, including cursor move, the client will send index, bytes that are going to be replaced and bytes that are being replaced.
 If server finds that bytes that are going to be replace differ at index+-10, it will reject the update request and client will revert the change because of a conflict (ignore the cursor marker).
 The server will perform the modification in the document and store a list of changes, each has its own revision#.
 A client will periodically poll changes since last poll, and update its view.
 
-Envelope
+###Envelope
 Add download support to create html document, of course this will not contain cursor locations.
 Add upload support of the html document into the document.
 Add authentication so each user has its own name/password, add user management capability to admin user, each user can be admin by adding attribute, store the users in XML format.
 Each document created will be auto saved in server side, owned by the user that created it, a user may list documents that belongs to him, and share with other users. The permissions will be saved in XML file.
 
-Finishes (Score: 100)
+###Finishes (Score: 100)
 Support <Up/Down> arrows.
 Support <Shift><Up/Down>.
 Support <PageUp/Down> and scrolling.
 Object oriented, documentation.
 
-Styles
+###Styles
 Text size?
 Images?
 Right/Left/Center?
@@ -51,6 +51,7 @@ etc...
 Encryption
 Encrypt file content on server when saving using a password.
 
+##References
 
 [1] http://www.w3schools.com/jsref/prop_html_innerhtml.asp
 [2] http://www.w3schools.com/tags/ev_onkeydown.asp
@@ -58,6 +59,6 @@ Encrypt file content on server when saving using a password.
 [4] http://www.w3schools.com/xml/xml_http.asp
 
 Helpful for learing keypress:
-https://www.w3.org/2002/09/tests/keys.html 
+https://www.w3.org/2002/09/tests/keys.html
 Js class:
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
---
+++ __main__.py
@@ -0,0 +1,353 @@
+# -*- coding: utf-8 -*-
+import argparse
+import base64
+import contextlib
+import Cookie
+import errno
+import os
+import random
+import socket
+import subprocess
+import sys
+import time
+import traceback
+import urlparse
+
+from . import constants
+from . import util
+
+
+# for debug
+EYECATCHER = '-' * 10 + 'EYECATCHER' + '-' * 10 + '\r\n'
+MIME_MAPPING = {
+    'html': 'text/html',
+    'png': 'image/png',
+    'txt': 'text/plain',
+}
+HTML_STDR_SIZE = len('<HTML><BODY></BODY></HTML>')
+
+
+def send_status(s, code, message, extra):
+    util.send_all(
+        s,
+        (
+            (
+                '%s %s %s\r\n'
+                'Content-Type: text/plain\r\n'
+                '\r\n'
+                'Error %s %s\r\n'
+                '%s'
+            ) % (
+                constants.HTTP_SIGNATURE,
+                code,
+                message,
+                code,
+                message,
+                extra,
+            )
+        ).encode('utf-8')
+    )
+
+
+def parse_args():
+    """Parse program argument."""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--bind-address',
+        default='0.0.0.0',
+        help='Bind address, default: %(default)s',
+    )
+    parser.add_argument(
+        '--bind-port',
+        default=constants.DEFAULT_HTTP_PORT,
+        type=int,
+        help='Bind port, default: %(default)s',
+    )
+    parser.add_argument(
+        '--base',
+        default='.',
+        help='Base directory to search fils in, default: %(default)s',
+    )
+    args = parser.parse_args()
+    args.base = os.path.normpath(os.path.realpath(args.base))
+    return args
+
+
+def send_html(
+    s,
+    body='',
+    status='200 OK',
+    headers=[]
+):
+    util.send_all(
+        s,
+        (
+            (
+                '%s %s\r\n'
+                'Content-Length: %s\r\n'
+                'Content-Type: %s\r\n'
+                'Cache-Control: no-cache, no-store, must-revalidate\r\n'
+                'Pragma: no-cache\r\n'
+                'Expires: 0\r\n'
+                '%s'
+                '\r\n'
+                '<HTML><BODY>'
+                '%s'
+                '</BODY></HTML>'
+            ) % (
+                constants.HTTP_SIGNATURE,
+                status,
+                HTML_STDR_SIZE+len(body),
+                MIME_MAPPING['html'],
+                ''.join(headers),
+                body,
+            )
+        ).encode('utf-8')
+    )
+
+
+def cookie_sep(cookies_str):
+    cookies = {}
+    for cookie in Cookie.BaseCookie(cookies_str).values():
+        cookies[cookie.key] = cookie.value
+
+    return cookies
+
+
+def clock(s):
+    body = time.strftime(
+        "%Y-%m-%d %H:%M:%S",
+        time.gmtime(),
+    )
+    send_html(s, body=body)
+
+
+def mul(s, uri_parse):
+    vals = urlparse.parse_qs(uri_parse.query)
+    ans = int(vals['a'][0]) * int(vals['b'][0])
+    body = str(ans)
+    send_html(s, body=body)
+
+
+def secret1(s, basic=''):
+    if basic:
+        user_pass = basic.split(':')
+        if (
+            len(user_pass) == 2 and
+            user_pass[0] == 'user1' and
+            user_pass[1] == 'pass1'
+        ):
+
+            body = 'welcome %s' % user_pass[0]
+            send_html(s, body=body)
+
+    else:
+        send_html(
+            s,
+            status='401 Unauthorized',
+            headers=['WWW-Authenticate: Basic realm="User Visible Realm"'],
+        )
+
+
+def counter(s, cookies={}):
+    if 'count' not in cookies:
+        count = 1
+    else:
+        count = int(cookies['count']) + 1
+
+    body = str(count)
+    header = 'Set-Cookie: count=%d\r\n' % count
+    send_html(s, body=body, headers=[header])
+
+
+def login(s, uri_parse, users):
+    vals = urlparse.parse_qs(uri_parse.query)
+    name, passward = vals['a'][0], vals['b'][0]
+    headers = ['Location: http://localhost:8080/secret2\r\n']
+
+    if (
+        name == 'user1' and
+        passward == 'pass1'
+    ):
+        rnd_id = random.randint(1000, 9999)
+        headers.append('Set-Cookie: rnd_id=%d\r\n' % rnd_id)
+        users[rnd_id] = name
+    else:
+        headers.append('Set-Cookie: rnd_id=0\r\n')
+
+    send_html(
+        s,
+        status='301 Moved Permanently',
+        headers=headers,
+    )
+
+
+def secret2(s, cookies, users):
+    if 'rnd_id' in cookies and int(cookies['rnd_id']) in users:
+        body = 'welcome user1'
+    else:
+        body = 'identifying went wrong'
+
+    send_html(s, body=body)
+
+
+def run_prog(s, uri_parse):
+    to_run = [
+        sys.executable,
+        str(uri_parse.path)[1:],
+    ]
+    for arg in urlparse.parse_qs(uri_parse.query).values():
+        to_run.append(arg[0])
+
+    send_html(s, body=subprocess.check_output(to_run))
+
+
+def main():
+    args = parse_args()
+    basic = ''
+    users = {}
+    cookies = {}
+
+    with contextlib.closing(
+        socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+        )
+    ) as sl:
+        sl.bind((args.bind_address, args.bind_port))
+        sl.listen(10)
+        while True:
+            s, addr = sl.accept()
+            with contextlib.closing(s):
+                status_sent = False
+                try:
+
+                    rest = bytearray()
+
+                    #
+                    # Parse request line
+                    #
+                    req, rest = util.recv_line(s, rest)
+                    req_comps = req.split(' ', 2)
+                    if req_comps[2] != constants.HTTP_SIGNATURE:
+                        raise RuntimeError('Not HTTP protocol')
+                    if len(req_comps) != 3:
+                        raise RuntimeError('Incomplete HTTP protocol')
+
+                    method, uri, signature = req_comps
+                    if method != 'GET' and method != 'POST':
+                        raise RuntimeError(
+                            "HTTP unsupported method '%s'" % method
+                        )
+                    #
+                    # Create a file out of request uri.
+                    # Be extra careful, it must not escape
+                    # the base path.
+                    #
+                    # NOTICE: os.path.normpath cannot be used checkout:
+                    # os.path.normpath(('/a/b', '/a/b1')
+                    #
+                    if not uri or uri[0] != '/':
+                        raise RuntimeError("Invalid URI")
+
+                    file_name = os.path.normpath(
+                        os.path.join(
+                            args.base,
+                            uri[1:],
+                        )
+                    )
+
+                    if file_name[:len(args.base) + 1] != os.path.join(
+                        args.base,
+                        '',
+                    ):
+                        raise RuntimeError("Malicious URI '%s'" % uri)
+
+                    #
+                    # Parse headers
+                    #
+                    for i in range(constants.MAX_NUMBER_OF_HEADERS):
+                        line, rest = util.recv_line(s, rest)
+                        if not line:
+                            break
+                        if line.find('Authorization: Basic') != -1:
+                            basic = base64.b64decode(
+                                line[len('Authorization: Basic'):],
+                            )
+                        elif line.find('Cookie: ') != -1:
+                            cookies = cookie_sep(str(line))
+
+                    else:
+                        raise RuntimeError('Too many headers')
+
+                    uri_parse = urlparse.urlparse(uri)
+
+                    if uri_parse.path == '/clock':
+                        clock(s)
+
+                    elif uri_parse.path == '/mul':
+                        mul(s, uri_parse)
+
+                    elif uri_parse.path == '/secret1':
+                        secret1(s, basic)
+
+                    elif uri_parse.path == '/counter':
+                        counter(s, cookies)
+
+                    elif uri_parse.path == '/login':
+                        login(s, uri_parse, users)
+
+                    elif uri_parse.path == '/secret2':
+                        secret2(s, cookies, users)
+
+                    elif uri_parse.path[-3:] == '.py':
+                        run_prog(s, uri_parse)
+
+                    else:
+                        with open(file_name, 'rb') as f:
+
+                            util.send_all(
+                                s,
+                                (
+                                    (
+                                        '%s 200 OK\r\n'
+                                        'Content-Length: %s\r\n'
+                                        'Content-Type: %s\r\n'
+                                        '\r\n'
+                                    ) % (
+                                        constants.HTTP_SIGNATURE,
+                                        os.fstat(f.fileno()).st_size,
+                                        MIME_MAPPING.get(
+                                            os.path.splitext(
+                                                file_name
+                                            )[1].lstrip('.'),
+                                            'application/octet-stream',
+                                        ),
+                                    )
+                                ).encode('utf-8')
+                            )
+                            #
+                            # Send content
+                            #
+                            while True:
+                                buf = f.read(constants.BLOCK_SIZE)
+                                if not buf:
+                                    break
+                                util.send_all(s, buf)
+                except IOError as e:
+                    traceback.print_exc()
+                    if not status_sent:
+                        if e.errno == errno.ENOENT:
+                            send_status(s, 404, 'File Not Found', e)
+                        else:
+                            send_status(s, 500, 'Internal Error', e)
+                except Exception as e:
+                    traceback.print_exc()
+                    if not status_sent:
+                        send_status(s, 500, 'Internal Error', e)
+
+if __name__ == '__main__':
+    main()
+
+# vim: expandtab tabstop=4 shiftwidth=4
